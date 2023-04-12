cf0ad6b2df1cfb1439b26bf3d624525d9211235f
NNOSS999 <70695330+NNOSS999@users.noreply.github.com>
2023-04-12 11:45:38 +0900

    0.2

---
+++ js/talk.js
@@ -0,0 +1,75 @@
+new TypeIt("#lqrlyx", {
+        loop: true,
+        cursorSpeed: 1000,
+        speed: 100
+    })
+    .type("雨欣宝宝最可爱了")
+    .pause(3000)
+    .delete(null, {
+        delay: 400
+
+    })
+    .type("超爱雨欣宝贝")
+    .pause(3000)
+    .delete(null, {
+        delay: 400
+
+    })
+    .type("想你想你想你")
+    .pause(3000)
+    .delete(null, {
+        delay: 400
+
+    })
+    .type("雨欣宝宝在干嘛")
+    .pause(3000)
+    .delete(null, {
+        delay: 400
+
+    })
+    .type("心里想的都是雨欣宝贝")
+    .pause(3000)
+    .delete(null, {
+        delay: 400
+
+    })
+    .type("雨！")
+    .pause(3000)
+    .delete(null, {
+        delay: 400
+
+    })
+    .type("比心比心比心比心比心")
+    .pause(3000)
+    .delete(null, {
+        delay: 400
+
+    })
+    .type("♥♥♥♥♥♥♥♥♥")
+    .pause(3000)
+    .delete(null, {
+        delay: 400
+
+    })
+    .type("想要那个")
+    .pause(3000)
+    .delete(null, {
+        delay: 400
+
+    })
+    .type("宝贝吃饭了嘛？")
+    .pause(3000)
+    .delete(null, {
+        delay: 400
+
+    })
+    .type("想亲亲了")
+    .pause(3000)
+    .go();
+
+new TypeIt('#talkTo', {
+    lifeLike: false,
+    cursorSpeed: 1000,
+    waitUntilVisible: false,
+    speed: 100
+}).go();
